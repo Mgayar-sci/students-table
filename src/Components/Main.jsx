1f48e59da794b5965e8cc1971d8150d601ddb7a5
Mgayar-Sci <melgayar@sci.cu.edu.eg>
2024-12-01 13:42:39 +0200

    Copied App.js into Main component Moved Table component to Components folder

---
+++ src/Components/Main.jsx
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import "./main.css";
+import Table from "./Table";
+import { useState } from "react";
+
+const Main = () => {
+  const [showTable, setShowTable] = useState(false);
+  const handleClick = () => {
+    setShowTable(!showTable);
+  };
+  return (
+    <div className="App">
+      <button onClick={handleClick}>{showTable ? "Hide" : "Show"} Table</button>
+      <header
+        className={
+          showTable
+            ? "App-header-hidden App-header"
+            : "App-header App-header-show"
+        }
+      >
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div className={showTable ? "table" : "table hide-table"}>
+        <Table />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
